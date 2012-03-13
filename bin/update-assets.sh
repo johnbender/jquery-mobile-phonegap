f56ece9de0ddcec5beeb319e20b5a52a89cf9ce0
John Bender <john.m.bender@gmail.com>
2012-03-13 14:04:15 -0700

    make use of master

--- bin/update-assets.sh
+++ bin/update-assets.sh
@@ -10,11 +10,20 @@ sha=$(git rev-parse HEAD)
 make docs
 cd ..
 
+git checkout master
+
 # TODO sort out if it's possible to directly reference the jquerymobile submodule
 cp jquery-mobile/compiled/jquery.mobile.js assets/js/
 cp jquery-mobile/compiled/jquery.mobile.css assets/css/
 cp jquery-mobile/js/jquery.js assets/js/
 
+# create the commit for the latest from jqm
 git add assets
 git commit -m "update assets from latest jquerymobile at $sha"
-git push phonegap
+
+# push the latest to build at phonegap
+git push phonegap master
+
+# TODO hit the phonegap build api to kick off the package builds
+# curl foo
+
