dc46bf60d3dc0e8d601000a89bf5bcca2223a8cb
John Bender <john.m.bender@gmail.com>
2012-03-13 14:50:07 -0700

    use tar upload instead of git repo

--- bin/update-assets.sh
+++ bin/update-assets.sh
@@ -1,29 +1,24 @@
+if [ -z "$PHONEGAP_PASS" ]; then
+  echo "PHONEGAP_PASS must be defined in the environment"
+  exit 1
+fi
+
 git submodule init
 git submodule update
-
-if ! git remote | grep -q phonegap; then
-  git remote add phonegap $GIT_URL
-fi
 
 cd jquery-mobile
 sha=$(git rev-parse HEAD)
 make docs
 cd ..
 
-git checkout master
-
 # TODO sort out if it's possible to directly reference the jquerymobile submodule
+echo "updating the application assets to the latest jqm"
 cp jquery-mobile/compiled/jquery.mobile.js assets/js/
 cp jquery-mobile/compiled/jquery.mobile.css assets/css/
 cp jquery-mobile/js/jquery.js assets/js/
 
-# create the commit for the latest from jqm
-git add assets
-git commit -m "update assets from latest jquerymobile at $sha"
+echo "creating app tar.gz"
+tar --exclude="jquery-mobile" --exclude=".git*" --exclude="update.*" -czvf "update.tar.gz" .
 
-# push the latest to build at phonegap
-git push phonegap master
-
-# TODO hit the phonegap build api to kick off the package builds
-# curl foo
-
+echo "pushing the updated app tar"
+curl -u nous.placidus@gmail.com:$PHONEGAP_PASS -X PUT -d 'data={"version":"0.1.0"}' https://build.phonegap.com/api/v1/apps/86563
