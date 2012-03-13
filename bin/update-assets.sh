10ba3ef4638e44af8c186237edbaca21ff79f237
John Bender <john.m.bender@gmail.com>
2012-03-13 13:39:34 -0700

    use public submodule url and add push to phonegap repo

--- bin/update-assets.sh
+++ bin/update-assets.sh
@@ -1,12 +1,20 @@
 git submodule init
 git submodule update
+
+if ! git remote | grep -q phonegap; then
+  git remote add phonegap $GIT_URL
+fi
+
 cd jquery-mobile
 sha=$(git rev-parse HEAD)
 make docs
 cd ..
-# TODO sort out if we can just directly reference the jquerymobile submodule
+
+# TODO sort out if it's possible to directly reference the jquerymobile submodule
 cp jquery-mobile/compiled/jquery.mobile.js assets/js/
 cp jquery-mobile/compiled/jquery.mobile.css assets/css/
 cp jquery-mobile/js/jquery.js assets/js/
+
 git add assets
 git commit -m "update assets from latest jquerymobile at $sha"
+git push phonegap
